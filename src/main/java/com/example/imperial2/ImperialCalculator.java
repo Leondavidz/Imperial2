8c29dc480c032ae163a5e9448884cf4db0718395
Leon <davidyu362000@gmail.com>
2022-10-14 13:48:33 +0200

    feature/gram:Klasse +Unit Test +Implementierung

---
+++ src/main/java/com/example/imperial2/ImperialCalculator.java
@@ -0,0 +1,7 @@
+package com.example.imperial2;
+
+public class ImperialCalculator {
+    public double gram(double ounce){
+        return ounce * 28.35;
+    }
+}
---
+++ src/main/java/com/example/imperial2/ImperialCalculatorController.java
@@ -0,0 +1,16 @@
+package com.example.imperial2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ImperialCalculatorController {
+    ImperialCalculator a = new ImperialCalculator();
+
+    @RequestMapping("/gram")
+    public double gram(@RequestParam double ounce){
+        return a.gram(ounce);
+    }
+}
